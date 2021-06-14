e3fa10f4dae0337abb2e46f32f73935c2980b14b
HamitMizrak <hamitmizrak@gmail.com>
2021-06-14 22:43:23 +0300

    sql sayfası eklendi

---
+++ src/main/java/com/hamit/utils/all.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA `angular_springboot` DEFAULT CHARACTER SET utf8 ;
+
+use angular_springboot;
+
+select * from register where register_id=1;
+
+insert register (register_name) values ("Deneme")
+
+delete from register where register_id=4
+
+UPDATE `angular_springboot`.`register` SET `register_name` = 'Deneme44' WHERE (`register_id` = '5');
+
+update register set register_name="Deneme6666" where register_id=5
