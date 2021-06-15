31df8b0b1ca822543e1f1d64b06a17a0b0dbd35f
HamitMizrak <hamitmizrak@gmail.com>
2021-06-15 14:37:46 +0300

    modelMapper eklendi (SpringBootApplication)

--- src/main/java/com/hamit/AngularSpringBootProjectApplication.java
+++ src/main/java/com/hamit/AngularSpringBootProjectApplication.java
@@ -1,16 +1,22 @@
 package com.hamit;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AngularSpringBootProjectApplication {
-
+	
 	// psvm
 	public static void main(String[] args) {
 		SpringApplication.run(AngularSpringBootProjectApplication.class, args);
 	}
-
-	// bean
-
+	
+	// model mapper
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+	
 }
