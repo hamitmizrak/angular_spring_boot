be7f47eaeedbe6128d8dbe74d1e36eb1197df522
HamitMizrak <hamitmizrak@gmail.com>
2021-06-15 13:23:30 +0300

    Register Servis eklendi

--- src/main/java/com/hamit/service/RegiterService.java
+++ src/main/java/com/hamit/service/RegiterService.java
@@ -1,32 +1,29 @@
-package com.hamit.api;
+package com.hamit.service;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-
 import com.hamit.dto.RegisterDto;
 
-public interface RegisterApi<T> {
+public interface RegiterService {
 	
 	// CRUD
 	
 	// GET ==> single select * from register where register_id=1;
-	public ResponseEntity<RegisterDto> getById(int id);
+	public RegisterDto getById(int id);
 	
 	// GET ==> multiple select * from ;
-	public ResponseEntity<List<RegisterDto>> getAllList();
+	public List<RegisterDto> getAllList();
 	
 	// POST ==> insert register (register_name) values ("Deneme")
-	public ResponseEntity<RegisterDto> getPost(T dto);
+	public RegisterDto getPost(RegisterDto dto);
 	
 	// DELETE ==> delete from register where register_id=4
-	public ResponseEntity<RegisterDto> getDelete(int id);
+	public RegisterDto getDelete(int id);
 	
 	// DELETE ==> delete from register
-	public ResponseEntity<RegisterDto> getDeleteAll();
+	public RegisterDto getDeleteAll();
 	
 	// UPDATE ==> update `angular_springboot`.`register` set `register_name` =
 	// 'Deneme44' WHERE (`register_id` = '5');
-	public ResponseEntity<RegisterDto> getUpdate(int id, RegisterDto dto);
-	
+	public RegisterDto getUpdate(int id, RegisterDto dto);
 }
