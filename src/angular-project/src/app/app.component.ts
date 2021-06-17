6a97aca4e18d874b4f135e3ae2641469239bdcf8
HamitMizrak <hamitmizrak@gmail.com>
2021-06-17 11:10:22 +0300

    jQuery eklendi

--- src/angular-project/src/app/app.component.ts
+++ src/angular-project/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare let alertify: any; //alertify
+
+declare let $: any; //jquery
+declare let jQuery: any; //jQuery
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-project';
+
+  ngOnInit(): void {
+    /*
+    //jQuery
+  $('#jQuery_button').click(function () {
+    alertify.success("Jquery Çalışıyor");
+    $('#jQuery_h1').css("color","red")
+  });*/
+  
+}
+
+  //jQuery Click
+  jQueryClickExamples() {
+    alertify.success('Jquery Çalışıyor');
+    $('#jQuery_h1').css('color', 'white');
+    $('#jQuery_h1').css('background-color', 'black');
+  }
+
+  //alertify
+  success() {
+    alertify.error('oldu');
+  }
 }
