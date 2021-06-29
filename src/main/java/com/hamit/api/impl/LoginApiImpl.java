b41ac0c4458b9478d6ac8b1f749ea958a589c7e4
HamitMizrak <hamitmizrak@gmail.com>
2021-06-29 22:51:25 +0300

    LoginApiImpl eklendi

--- src/main/java/com/hamit/api/impl/LoginApiImpl.java
+++ src/main/java/com/hamit/api/impl/LoginApiImpl.java
@@ -2,6 +2,8 @@ package com.hamit.api.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hamit.api.LoginApi;
 import com.hamit.dto.LoginDto;
+import com.hamit.service.impl.LoginServiceImpl;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -42,52 +45,63 @@ public class LoginApiImpl implements LoginApi {
 	// PutMapping ==> @PathVariable @ModelAttribute
 	// DeleteMapping
 	
+	@Autowired
+	LoginServiceImpl loginServiceImpl;
+	
+	// localhost:9293/api/login/get/1
 	@Override
 	@GetMapping("/get/{login_id}")
 	@ApiOperation(value = "get", notes = "id ile tek bir tane veri dönder ", response = LoginDto.class)
 	public ResponseEntity<LoginDto> getById(@PathVariable("login_id") int id) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info(LoginDto.class + " Tek liste donuldu");
+		return ResponseEntity.ok(loginServiceImpl.getById(id));
 	}
 	
+	// localhost:9293/api/login/get/list
 	@Override
 	@GetMapping("/get/list")
 	@ApiOperation(value = "", notes = "listeleme yapıalcak", response = LoginDto.class)
 	public ResponseEntity<List<LoginDto>> getAllList() {
-		// TODO Auto-generated method stub
-		return null;
+		log.info(LoginDto.class + " Tüm listeler donuldu");
+		return ResponseEntity.ok(loginServiceImpl.getAllList());
 	}
 	
+	// localhost:9293/api/login/update/1
 	@Override
 	@PutMapping("/update/{login_id}")
 	@ApiOperation(value = "Put", notes = "güncelleme id ve nesne", response = LoginDto.class)
 	public ResponseEntity<LoginDto> getUpdate(@PathVariable("login_id") int id, @ModelAttribute LoginDto loginDto) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info(LoginDto.class + " Guncelendi");
+		return ResponseEntity.ok(loginServiceImpl.getUpdate(id, loginDto));
 	}
 	
+	// localhost:9293/api/login/delete/1
 	@Override
-	@DeleteMapping("/delete/{login_id")
+	@DeleteMapping("/delete/{login_id}")
 	@ApiOperation(value = "delete By Id", notes = "id ile silme yapılacak ", response = LoginDto.class)
 	public ResponseEntity<LoginDto> getDelete(@PathVariable("login_id") int id) {
-		// TODO Auto-generated method stub
-		return null;
+		loginServiceImpl.getDelete(id);
+		log.info(LoginDto.class + " Silindi");
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
+	// localhost:9293/api/login/delete/list
 	@Override
 	@DeleteMapping("/delete/list")
 	@ApiOperation(value = "delete allList", notes = "dikkat bütün tablo silinir", response = LoginDto.class)
 	public ResponseEntity<LoginDto> getAllDelete() {
 		// TODO Auto-generated method stub
-		return null;
+		log.info(LoginDto.class + " Bütün tablodaki veriler silindi");
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
+	// localhost:9293/api/login/post
 	@Override
 	@PostMapping("/post")
 	@ApiOperation(value = "post", notes = "ekleme yapılacak", response = LoginDto.class)
 	public ResponseEntity<LoginDto> getPost(@ModelAttribute LoginDto loginDto) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info(LoginDto.class + " Eklendi");
+		return ResponseEntity.ok(loginServiceImpl.getPost(loginDto));
 	}
 	
 }
